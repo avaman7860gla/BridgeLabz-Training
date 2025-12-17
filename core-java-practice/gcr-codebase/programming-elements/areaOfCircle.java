f5ac8b61881f94086aef355c5cc4ba27c9fe01be
Aman <avaman7860@gmail.com>
2025-12-17 15:42:05 +0530

    [Aman Verma] Add areaOfCircle Java program

---
+++ core-java-practice/gcr-codebase/programming-elements/areaOfCircle.java
@@ -0,0 +1,7 @@
+class areaOfCircle{
+    public static void main(String args[]){
+        double radius = 7.5;
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}

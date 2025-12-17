9db2db1487e42da4c83ba735d7ea5cc64b5c099c
Aman <avaman7860@gmail.com>
2025-12-17 21:14:54 +0530

    [Aman Verma] Add palindromeNumber Java program

---
+++ core-java-practice/leet-code-codebase/palindromeNumber.java
@@ -0,0 +1,15 @@
+class palindromeNumber {
+    public static boolean isPalindrome(int x) {
+        int n=x;
+        int r,s=0;
+        while(x>0){
+            r=x%10;
+            s=s*10+r;
+            x=x/10;
+        }
+        if(s==n){
+            return true;
+        }
+        return false;
+    }
+}

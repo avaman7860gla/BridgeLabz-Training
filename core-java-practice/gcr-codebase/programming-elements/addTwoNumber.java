5c7ec1b2fce622479671f875c795fda70a84c7b2
Aman <avaman7860@gmail.com>
2025-12-17 15:42:04 +0530

    [Aman Verma] Add addTwoNumber Java program

---
+++ core-java-practice/gcr-codebase/programming-elements/addTwoNumber.java
@@ -0,0 +1,8 @@
+class  addTwoNumber{
+    public static void main(String args[]){
+        int num1 = 1;
+        int num2 = 2;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}

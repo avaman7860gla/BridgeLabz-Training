c3a816d036f44bdf57b73d7227df94c2203a5f0d
Aman <avaman7860@gmail.com>
2025-12-17 15:42:05 +0530

    [Aman Verma] Add celciusToFahrenheit Java program

---
+++ core-java-practice/gcr-codebase/programming-elements/celciusToFahrenheit.java
@@ -0,0 +1,8 @@
+class celciusToFahrenhiet{
+    public static void main(String args[]){
+        double celsius = 12.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println("Celsius: " + celsius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}

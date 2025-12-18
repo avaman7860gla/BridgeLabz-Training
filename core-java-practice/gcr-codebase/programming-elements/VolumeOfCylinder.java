946b48402581ab8d9868da20227da245a900767e
Aman <avaman7860@gmail.com>
2025-12-18 17:01:05 +0530

    [Aman Verma] completed all rest questions

---
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfCylinder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VolumeOfCylinder {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double radius=sc.nextDouble();
+		double height=sc.nextDouble();
+		
+		double volume=3.14*radius*radius*height;
+		
+		System.out.println(volume);
+
+	}
+
+}

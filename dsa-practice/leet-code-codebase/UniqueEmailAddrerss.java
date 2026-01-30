58a72ac4f41a5f72fb85e86567b94ffed8596a59
Aman <avaman7860@gmail.com>
2026-01-30 23:07:40 +0530

    [Aman Verma] Add. leetcode Problems

---
+++ dsa-practice/leet-code-codebase/UniqueEmailAddrerss.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class UniqueEmailAddrerss {
+    public static int numUniqueEmails(String[] emails) {
+        HashSet<String> uniqueEmails = new HashSet<>();
+        for (String email : emails) {
+            StringBuilder sb = new StringBuilder();
+            boolean ignore = false, atEnd = false;
+            char[] emailStr = email.toCharArray();
+            for(char ch : emailStr) {
+                if (!atEnd && ch == '+') {
+                    ignore = true;
+                }
+                else if (ch == '@') {
+                    ignore = false;
+                    atEnd = true;
+                }
+                else if (!atEnd && ch == '.') {
+                    continue;
+                }
+                if (!ignore) {
+                    sb.append(ch);
+                }
+            }
+            uniqueEmails.add(sb.toString());
+        }
+        return uniqueEmails.size();
+    }
+}

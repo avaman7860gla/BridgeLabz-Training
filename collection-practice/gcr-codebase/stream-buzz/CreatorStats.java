b06d7f49bf39c6e965c82a92a8aa11c60c788a44
Aman <avaman7860@gmail.com>
2026-02-02 15:34:27 +0530

    [Aman Verma] Add. stream buzz scenerio based problem

---
+++ collection-practice/gcr-codebase/stream-buzz/CreatorStats.java
@@ -0,0 +1,10 @@
+class CreatorStats {
+    public String creatorName;
+    public double[] weeklyLikes;
+
+    // Constructor
+    public CreatorStats(String creatorName, double[] weeklyLikes) {
+        this.creatorName=creatorName;
+        this.weeklyLikes=weeklyLikes;
+    }
+}

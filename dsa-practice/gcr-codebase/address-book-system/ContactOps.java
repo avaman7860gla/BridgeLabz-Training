f65e6737b166f526518a8a03a10ed884c351c9b8
Aman <avaman7860@gmail.com>
2026-02-16 15:36:35 +0530

    [Aman Verma] Add . Restructured address book project folder structure

--- dsa-practice/gcr-codebase/address-book-system/ContactOps.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.ArrayList;
-
-interface ContactOps{
-    void addContact(Contact contact);
-    void editContact(String firstName, String lastName, Contact updatedContact);
-    void deleteContact(String firstName, String lastName);
-    List<Contact> getContacts();
-}

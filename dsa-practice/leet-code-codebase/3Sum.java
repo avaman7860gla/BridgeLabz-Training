411797ac60c400c34302aa08f55a5d97c5b71f8e
Aman <avaman7860@gmail.com>
2026-02-19 20:55:49 +0530

    [Aman Verma] Add. solved leetcode problems

---
+++ dsa-practice/leet-code-codebase/3Sum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class 3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>>list=new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum>0){
+                    r--;
+                }
+                else if(sum<0){
+                    l++;
+                }
+                else{
+                    list.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    while(l<r && nums[l]==nums[l-1]){
+                        l++;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
